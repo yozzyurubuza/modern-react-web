fdaadcd9b7ef37644472ef749c7b3cda990bacae
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-15 05:27:06 +0800

    Added codes about Using @faker-js to Generate Dummy Avatars

--- 03-communicating-with-props/src/index.js
+++ 03-communicating-with-props/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "@faker-js/faker";
 
 const App = function () {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
