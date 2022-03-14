6cfd5051217a7f39953379cba61cf5c4785a76b8
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-15 06:29:06 +0800

    Added codes about Implementing an Approval Card

--- 03-communicating-with-props/src/index.js
+++ 03-communicating-with-props/src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "@faker-js/faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = function () {
   return (
     <div className="ui container comments">
       {/* Syntax for calling CommentDetail */}
+      <ApprovalCard />
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:45PM"
