62b99e3c26177fd3671b3f0b1d498d7b041ea669
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-15 02:49:26 +0800

    Added Starter Files

---
+++ 03-communicating-with-props/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react";
+
+const App = function () {
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
