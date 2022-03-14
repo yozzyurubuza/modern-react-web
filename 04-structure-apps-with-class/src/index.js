30cee6dc7b006b2c258f164519d876cb89215bda
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-15 07:28:40 +0800

    Added starter files

---
+++ 04-structure-apps-with-class/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = function () {
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
