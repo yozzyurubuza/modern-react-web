34796c5b04c80c43f44ab64b263dad81a70ab1fd
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-15 06:18:49 +0800

    Added codes about Passing Props

--- 03-communicating-with-props/src/index.js
+++ 03-communicating-with-props/src/index.js
@@ -7,9 +7,24 @@ const App = function () {
   return (
     <div className="ui container comments">
       {/* Syntax for calling CommentDetail */}
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        displaypic={faker.image.avatar()}
+        comment="Nice blog post!"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        displaypic={faker.image.avatar()}
+        comment="You look very nice!"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        displaypic={faker.image.avatar()}
+        comment="Amazing Content!"
+      />
     </div>
   );
 };
