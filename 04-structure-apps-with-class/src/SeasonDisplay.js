277081c2b2d33f09df438efc38664b814ca505e8
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-16 03:30:11 +0800

    Refactoring and Extracting Options to Config Objects

--- 04-structure-apps-with-class/src/SeasonDisplay.js
+++ 04-structure-apps-with-class/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr, it's cold",
+    iconName: "snowflake",
+  },
+};
+
 const getSeason = function (lat, month) {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -10,15 +21,14 @@ const getSeason = function (lat, month) {
 
 const SeasonDisplay = function (props) {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === "winter" ? "Burr, it's chilly" : "Let's hit the beach!";
-  const icon = season === "winter" ? "snowflake" : "sun";
+  //Get text and iconName from object seasonConfig using destructure and bracket notation.
+  const { text, iconName } = seasonConfig[season];
 
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
