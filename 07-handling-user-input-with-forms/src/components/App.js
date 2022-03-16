6cc1636f721016e0447a3064874145414446b5f4
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-17 03:55:22 +0800

    Added codes about Handling Requests with Async Await

--- 07-handling-user-input-with-forms/src/components/App.js
+++ 07-handling-user-input-with-forms/src/components/App.js
@@ -3,13 +3,19 @@ import axios from "axios"; //Put import of 3rd-party libraries above components
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const res = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID 3v4g1r6C3nwy6_odUNZJXLeIcwK-4YGQ0uXBiRZm2u0",
       },
     });
+    // Other approach to get data
+    //   .then((res) => {
+    //     console.log(res.data.results);
+    //   });
+
+    console.log(res.data.results);
   }
 
   render() {
