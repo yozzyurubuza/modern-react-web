6507969262a0c174aa870b3f32cc2ca23e7e7e8b
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-18 02:37:03 +0800

    Added codes for Styling VideoDetail

--- 11-test-react-mastery/src/components/VideoDetail.js
+++ 11-test-react-mastery/src/components/VideoDetail.js
@@ -3,7 +3,14 @@ import React from "react";
 const VideoDetail = function ({ video }) {
   if (!video) return <div>Loading....</div>;
 
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div>
+      <div className="ui segment">
+        <h4>{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
