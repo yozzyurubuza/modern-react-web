9c2fe217f3c849335e99d23f9098c392285d1e89
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-17 23:52:24 +0800

    Added codes for Implementing a Search Bar

--- 11-test-react-mastery/src/components/SearchBar.js
+++ 11-test-react-mastery/src/components/SearchBar.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-class SearchBar extends React.Component{
-    render(){
-        return <div>SearchBar</div>
-    }
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = () => {};
+
+  render() {
+    return (
+      <div className="search-bar ui segment" style={{ marginTop: 10 }}>
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              onChange={this.onInputChange}
+              value={this.state.term}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
