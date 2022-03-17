fc8fc536d825e4cb8c1ed515fe795f54a18e3169
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-17 20:03:05 +0800

    Added codes about Creating an Image Card Component

--- 07-handling-user-input-with-forms/src/components/ImageList.js
+++ 07-handling-user-input-with-forms/src/components/ImageList.js
@@ -1,10 +1,11 @@
 import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = function (props) {
-  const images = props.images.map(({ description, id, urls }) => {
+  const images = props.images.map((image) => {
     //Assign key to the root of element only
-    return <img alt={description} key={id} src={urls.regular} />;
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
 };
