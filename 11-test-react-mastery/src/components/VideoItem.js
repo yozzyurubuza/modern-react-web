f2771d355a4d07fa0b65efa80ced0c88f913f8aa
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-18 01:23:26 +0800

    Added codes for Rendering a Video Thumbnail

--- 11-test-react-mastery/src/components/VideoItem.js
+++ 11-test-react-mastery/src/components/VideoItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const VideoItem = function (props) {
-  return <div>VideoItem</div>;
+const VideoItem = function ({ video }) {
+  return (
+    <div>
+      <img
+        alt={video.snippet.title}
+        src={video.snippet.thumbnails.medium.url}
+      />
+      {video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;
