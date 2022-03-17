fcdbe6f9b4abe89cdcc3e86eed07d798afc1755b
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-18 01:53:51 +0800

    Added codes and styling on displaying Video Items

--- 11-test-react-mastery/src/components/VideoItem.js
+++ 11-test-react-mastery/src/components/VideoItem.js
@@ -1,13 +1,17 @@
+import "./VideoItem.css";
 import React from "react";
 
 const VideoItem = function ({ video }) {
   return (
-    <div>
+    <div className="video-item item">
       <img
+        className="ui image"
         alt={video.snippet.title}
         src={video.snippet.thumbnails.medium.url}
       />
-      {video.snippet.title}
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
   );
 };
