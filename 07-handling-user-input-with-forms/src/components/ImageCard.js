3eb51e90bc585ffd8962e5680a9ac4bac84c87ba
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-17 22:36:44 +0800

    Added codes about Dynamic Spans

--- 07-handling-user-input-with-forms/src/components/ImageCard.js
+++ 07-handling-user-input-with-forms/src/components/ImageCard.js
@@ -3,6 +3,7 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
 
     this.imageRef = React.createRef();
   }
@@ -10,14 +11,21 @@ class ImageCard extends React.Component {
   componentDidMount() {
     //document.querySelector('.img').clientHeight
     //React way:
-    console.log(this.imageRef.current.clientHeight);
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
 
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
