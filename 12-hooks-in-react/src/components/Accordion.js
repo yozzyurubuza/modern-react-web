fa0a3b3c240e632600bdda74774cda36a21b6a89
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-24 18:32:14 +0800

    Add helper function in function components

--- 12-hooks-in-react/src/components/Accordion.js
+++ 12-hooks-in-react/src/components/Accordion.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
+  const onTitleClick = (index) => {
+    console.log("Title clicked", index);
+  };
+
   const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={index}>
-        <div className="title active">
+        <div className="title active" onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
