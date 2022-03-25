5eaa0cdbf0c0958d0452b3f9c47b058fbd5f5f7f
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-25 09:33:57 +0800

    Add codes about expanding the accordion

--- 12-hooks-in-react/src/components/Accordion.js
+++ 12-hooks-in-react/src/components/Accordion.js
@@ -4,31 +4,31 @@ const Accordion = ({ items }) => {
   //Assign useState(null) to the 2 elements.
   // setActiveIndex - function to update state, re-render
   const [activeIndex, setActiveIndex] = useState(null);
+  const [lastActive, setLastActive] = useState(null);
 
   const onTitleClick = (index) => {
+    //After running this code, component will re-render by going back on top.
+    //Default value will no longer be null.
     setActiveIndex(index);
   };
 
   const renderedItems = items.map((item, index) => {
+    let active = index === activeIndex ? "active" : "";
+
     return (
       <React.Fragment key={index}>
-        <div className="title active" onClick={() => onTitleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
