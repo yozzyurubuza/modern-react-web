c175cec131204dedbacefc4fd5165b64d6cd34d4
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-28 09:43:06 +0800

    Add codes about Async Code in useEffect

--- 12-hooks-in-react/src/components/Search.js
+++ 12-hooks-in-react/src/components/Search.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
 
   console.log("I RUN WITH EVERY RENDER");
 
+  //Cannot use async in useEffect
   useEffect(() => {
-    console.log("I ONLY RUN AFTER EVERY RENDER AND AT INITIAL RENDER");
-  });
+    //1. Create a new variable
+    const search = async () => {
+      await axios.get("adssadas");
+    };
+    //2. Immediately Invoked Function Expression
+    (async () => {
+      await axios.get("adssadas");
+    })();
+
+    //3. Normal promises
+    axios.get("asdsadsa").then((res) => {
+      console.log(res);
+    });
+  }, [term]);
 
   return (
     <div>
