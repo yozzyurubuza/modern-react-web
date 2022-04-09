885342c4e4f99446cfcf7d7e9b3000659efbc645
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-10 00:11:40 +0800

    Edit Label

--- 11-test-react-mastery/src/components/SearchBar.js
+++ 11-test-react-mastery/src/components/SearchBar.js
@@ -20,7 +20,7 @@ class SearchBar extends React.Component {
       <div className="search-bar ui segment" style={{ marginTop: 10 }}>
         <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
-            <label>Video Search</label>
+            <label>Video Search Bar</label>
             <input
               type="text"
               onChange={this.onInputChange}
