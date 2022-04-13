372712a9403d11224d884d52a88d4a697f8d4c55
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-13 17:34:25 +0800

    Disable Timer Component

--- 12-hooks-in-react/src/App.js
+++ 12-hooks-in-react/src/App.js
@@ -9,31 +9,11 @@ const App = () => {
   const [selected, setSelected] = useState(OPTIONS[0]);
   const [showDropdown, setShowDropdown] = useState(SHOWCOMPONENT);
 
-  const [showTimer, setShowTimer] = useState(SHOWCOMPONENT);
-  const [time, setTime] = useState(TIME);
-
   return (
     <div>
       <button onClick={() => setShowDropdown(!showDropdown)}>
         Toggle Dropdown
       </button>
-      <button
-        onClick={() => {
-          setShowTimer(!showTimer);
-          setTime(TIME);
-        }}
-      >
-        Toggle Timer
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={OPTIONS}
-        />
-      ) : null}
-
-      <div>{showTimer ? <Timer time={time} setTime={setTime} /> : null}</div>
     </div>
   );
 };
