e3e5e11b4b148fb08dbaadcf7930fa22a38e9a07
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-13 18:21:39 +0800

    Add Convert Component

--- 12-hooks-in-react/src/components/Translate.js
+++ 12-hooks-in-react/src/components/Translate.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 import { OPTIONS_LANG } from "../Config";
+
+const key = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
 
 const Translate = () => {
   const [language, setLanguage] = useState(OPTIONS_LANG[0]);
@@ -20,6 +23,9 @@ const Translate = () => {
         selected={language}
         onSelectedChange={setLanguage}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
     </div>
   );
 };
