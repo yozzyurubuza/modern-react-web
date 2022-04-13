72fd91e6def30631b00dbc85e1bd3a34ed75d324
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-13 18:09:27 +0800

    Add Text input field

--- 12-hooks-in-react/src/components/Translate.js
+++ 12-hooks-in-react/src/components/Translate.js
@@ -4,10 +4,18 @@ import { OPTIONS_LANG } from "../Config";
 
 const Translate = () => {
   const [language, setLanguage] = useState(OPTIONS_LANG[0]);
+  const [text, setText] = useState("");
 
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)}></input>
+        </div>
+      </div>
       <Dropdown
+        label="Select a Language"
         options={OPTIONS_LANG}
         selected={language}
         onSelectedChange={setLanguage}
