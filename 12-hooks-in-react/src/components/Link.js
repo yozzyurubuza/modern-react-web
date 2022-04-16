3472a2d95e00d6215a57d1c02da92948dae92766
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-16 17:25:27 +0800

    Add Link Component

---
+++ 12-hooks-in-react/src/components/Link.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Link = ({ className, href, children }) => {
+  const onClick = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <a
+      onClick={onClick}
+      className={className}
+      href={href}
+      children={children}
+    ></a>
+  );
+};
+
+export default Link;
