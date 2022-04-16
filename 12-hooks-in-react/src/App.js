382ce6bf5fdbbcf3290d5f9f9859646e8194770b
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-16 17:00:51 +0800

    Add basic routing component

--- 12-hooks-in-react/src/App.js
+++ 12-hooks-in-react/src/App.js
@@ -6,10 +6,29 @@ import Timer from "./components/Timer";
 import Translate from "./components/Translate";
 import { TIME, OPTIONS, ITEMS, SHOWCOMPONENT } from "./Config";
 
+const showAccordion = () => {
+  if (window.location.pathname === "/") return <Accordion items={ITEMS} />;
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") return <Search />;
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") return <Dropdown />;
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") return <Translate />;
+};
+
 const App = () => {
   return (
     <div>
-      <Translate />
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
     </div>
   );
 };
