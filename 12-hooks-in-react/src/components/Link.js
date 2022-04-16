b9faeba962f1ab483c10d9a33adb97a31a693dcf
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-16 17:41:48 +0800

    Add Ctrl+click function

--- 12-hooks-in-react/src/components/Link.js
+++ 12-hooks-in-react/src/components/Link.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    // Enable ctrl + click to open link in new tab
+    if (event.metaKey || event.ctrlKey) return;
+
     event.preventDefault();
     window.history.pushState({}, "", href);
 
     //Communicate to route event change
-    const navEvent = new PopStateEvent('popstate');
+    const navEvent = new PopStateEvent("popstate");
     window.dispatchEvent(navEvent);
   };
 
